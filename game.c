38ef2d4975f24493b665755e24298203384dbf36
Christophe Casino <ccasino50@stuy.edu>
2025-01-07 14:44:08 -0500

    added game make arguement processor

--- game.c
+++ game.c
@@ -1,1 +1,15 @@
 #include "wordle.h"
+
+int main(int argc, char *argv[]){
+  
+  if(strcmp(argv[1], "guess") == 0){
+    guess_function();
+  } else if(strcmp(argv[1], "answer") == 0){
+    answer_function();
+  } else {
+    printf("Error, none of the arguments are valid\n");
+    return 1;
+  }
+
+  return 0;
+}
