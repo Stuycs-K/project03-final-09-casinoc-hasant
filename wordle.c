60be825a82e0806e088864b79bb6a4d8e4ae3128
Christophe Casino <ccasino50@stuy.edu>
2025-01-08 14:39:25 -0500

    added answer function with base variables from semaphore lab and first semaphore operation

--- wordle.c
+++ wordle.c
@@ -1,1 +1,29 @@
 #include "wordle.h"
+
+void guess_function(){
+  FILE *fd;
+  int *data;
+  int semd;
+  int shmd;
+}
+
+void answer_function(){
+  struct sembuf sb;
+  FILE *fd;
+  int semd;
+  int shmd;
+  int *bytes_wrote;
+  int bytes_writing;
+  int seeking;
+  char input[200];
+  char last_line[200];
+
+  printf("Waiting for Guesser to make a guess...\n");
+  semd = semget(SEMKEY, 1, 0);
+  sb.sem_num = 0;
+  sb.sem_flg = SEM_UNDO;
+  sb.sem_op = -1;
+  semop(semd, &sb, 1);
+  shmd = shmget(SHMKEY, 0, 0);
+  bytes_wrote = shmat(shmd, 0, 0);
+}
