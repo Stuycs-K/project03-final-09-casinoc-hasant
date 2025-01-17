32285c6a2338e9a5399a025c34bf08f7af53c014
Christophe Casino <ccasino50@stuy.edu>
2025-01-16 21:06:35 -0500

    added random int printing back to main file to start from somewhere

--- init.c
+++ init.c
@@ -28,6 +28,7 @@ static void sighandler(int signo){
 int main(int argc, char *argv[]) {
   signal(SIGINT, sighandler);
   signal(SIGPIPE, sighandler);
+  srand(time(NULL));
 
   int to_client; // Private
   int from_client; // WKP
@@ -36,17 +37,36 @@ int main(int argc, char *argv[]) {
   int WKPfd;
   if (argc > 1) {
     if(strcmp(argv[1], "Player1") == 0){
-      to_client = server_setup();
+      /* to_client = server_setup();
       WKPfd = to_client;
-      from_client = server_handshake( &to_client );
-      //open("answer1.txt", O_RDWR | O_CREAT, 0644);
-      //open("guess1.txt", O_RDWR | O_CREAT, 0644);
-      
+      from_client = server_handshake( &to_client ); */
+      while(1){
+        to_client = server_setup();
+        WKPfd = to_client;
+        from_client = server_handshake( &to_client );
+
+        while(1){
+          int random = (rand() % 101);
+          char sent_int[20];
+          sprintf(sent_int, "%d", random);
+          int n = write(to_client, sent_int, sizeof(sent_int));
+          if(n < 0){
+            close(to_client);
+            to_client = WKPfd;
+            break;
+          }
+          sleep(1);
+        }
+      }
     }
     else if(strcmp(argv[1], "Player2") == 0){
       from_server = client_handshake( &to_server );
-      //open("answer2.txt", O_RDWR | O_CREAT, 0644);
-      //open("guess2.txt", O_RDWR | O_CREAT, 0644);
+      while(1){
+        char recieved_int[100];
+        read(from_server, recieved_int, sizeof(recieved_int));
+        printf("recieved_int %s\n", recieved_int);
+        sleep(1);
+      }
     }
     else {
       printf("Please enter either Player1 or Player2\n");
@@ -55,11 +75,11 @@ int main(int argc, char *argv[]) {
   }
   else {
     printf("Please give an argument\n");
-    char guess[256];
+    char guess[5];
     char * returning;
 
     for(int i = 1; i <= 6; i++){
-        printf("Enter guess: ");
+        printf("Enter guess: \n");
         fgets(guess, sizeof(guess), stdin);
         returning = wordle_function(guess);
         printf("returned: %s\n", returning);
@@ -71,6 +91,7 @@ int main(int argc, char *argv[]) {
     }
     printf("Game over, you are out guesses.\n");
     free(returning);
+
   }
   return 0;
 }
