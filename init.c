92c3e414b47ca94403cff0c5c7bb4e22b51158d1
Christophe Casino <ccasino50@stuy.edu>
2025-01-18 13:44:58 -0500

    starting to add the wordle compatability with the server and client functionality

--- init.c
+++ init.c
@@ -37,36 +37,50 @@ int main(int argc, char *argv[]) {
   int WKPfd;
   if (argc > 1) {
     if(strcmp(argv[1], "Player1") == 0){
-      /* to_client = server_setup();
+      to_client = server_setup();
       WKPfd = to_client;
-      from_client = server_handshake( &to_client ); */
-      while(1){
-        to_client = server_setup();
-        WKPfd = to_client;
-        from_client = server_handshake( &to_client );
+      from_client = server_handshake( &to_client );
 
-        while(1){
-          int random = (rand() % 101);
-          char sent_int[20];
-          sprintf(sent_int, "%d", random);
-          int n = write(to_client, sent_int, sizeof(sent_int));
-          if(n < 0){
-            close(to_client);
-            to_client = WKPfd;
-            break;
+      char guess[7]; // Needs to be size 7 because of new line and null byte characters.
+      char * returning;
+
+      for(int i = 1; i <= 6; i++){
+          printf("Enter guess: \n");
+          fgets(guess, sizeof(guess), stdin);
+          returning = wordle_function(guess);
+          printf("returned: %s\n", returning);
+          if(strcmp(returning, "TESTS") == 0){
+            printf("You won in %d guesses!\n", i);
+            free(returning);
+            return 0;
           }
-          sleep(1);
-        }
       }
+      printf("Game over, you are out guesses.\n");
+      free(returning);
+
+      /* while(1){
+        int random = (rand() % 101);
+        char sent_int[20];
+        sprintf(sent_int, "%d", random);
+        int n = write(to_client, sent_int, sizeof(sent_int));
+        if(n < 0){
+          close(to_client);
+          to_client = WKPfd;
+          break;
+        }
+        sleep(1);
+      } */
     }
     else if(strcmp(argv[1], "Player2") == 0){
       from_server = client_handshake( &to_server );
-      while(1){
+
+      
+      /* while(1){
         char recieved_int[100];
         read(from_server, recieved_int, sizeof(recieved_int));
         printf("recieved_int %s\n", recieved_int);
         sleep(1);
-      }
+      } */
     }
     else {
       printf("Please enter either Player1 or Player2\n");
@@ -75,7 +89,7 @@ int main(int argc, char *argv[]) {
   }
   else {
     printf("Please give an argument\n");
-    char guess[5];
+    char guess[7];//Needs to be size 7 because of new line and null byte characters.
     char * returning;
 
     for(int i = 1; i <= 6; i++){
