cf64d50d9f12fc390b5b921454742e9d250ab923
Christophe Casino <ccasino50@stuy.edu>
2025-01-18 15:59:04 -0500

    added server and client handshake to header file

--- wordle.h
+++ wordle.h
@@ -3,6 +3,8 @@
 #define SEMKEY 2195
 #define SHMKEY 2196
 int server_setup();
+int server_handshake(int *to_client);
+int client_handshake(int *to_server);
 int * letter_counter(char * word);
 char* wordle_function(char* guess, char* answer);
 #endif
